da5197cc6851d3be863b662c7501afa0454a1077
Alaine12 <freytasalayne42@gmail.com>
2023-03-01 08:21:48 -0300

    primeiro envio

---
+++ l1q5.py
@@ -0,0 +1,4 @@
+#faça um programa que converta metros para centímetros.
+#lm tem 100cm
+#entrar
+tamanho_em
--- l1q9.py
+++ l1q9.py
@@ -1,4 +1,5 @@
 #faça um programa que peça a temperatura em graus fahrenheit, trasforme e mostre a temperatura em graus celsius.
 # c = 5 *  ((F- 32) / 9).
 #entrada
-temperatura_fahrenheit = float
+temperatura_fahrenheit = float(input('temperatura em fahrenheit'))
+
